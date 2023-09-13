dd472c431fc083bf651cf734bf3109510968b5a4
noruzzamanrubel <noruzzamanrubel@gmail.com>
2023-09-13 09:39:35 +0600

    fix shop page setting

--- includes/backend/class-qcfw-checkout-settings.php
+++ includes/backend/class-qcfw-checkout-settings.php
@@ -240,6 +240,7 @@ class Qcfw_Checkout_Settings {
                 )
             ) );
         
+            
             /** Button Settings */
             CSF::createSection( $prefix, array(
             'name'   => 'qcfw_button_settings',
